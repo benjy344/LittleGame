82e8e5d9697aa0109604642ba74df4a244934340
Benjamin DEMAIZIERE <demaiziere.benjamin@gmail.com>
2017-03-16 23:51:46 +0100

    can exit from a dunjon, monsters drop random gold, Avatar loose 40% gold if GameOver

--- app/controllers/index_controller.rb
+++ app/controllers/index_controller.rb
@@ -1,8 +1,8 @@
 class IndexController < ApplicationController
 
-
-
 	def gameover
-		
+		@avatar = current_user.avatar
+		@gold = @avatar.money - (@avatar.money*0.4)
+		@avatar.update(money: @gold);
 	end
 end
